55a04ef899fbae408c7e12b0b934042b15a4555e
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-07-04 12:33:13 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+import "dotenv/config";
 
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
